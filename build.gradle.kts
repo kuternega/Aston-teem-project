53aef9a21558baffbe045cd25af0a9ad3f0e1041
Vladislav <rumas123496@gmail.com>
2026-03-03 12:48:59 +0300

    Исправлены зависимости для JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
 }
 
 tasks.test {
